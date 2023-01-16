95d0ba76f3f27748387c05aaaf73d708d1635b9f
alexhou83 <120533655+alexhou83@users.noreply.github.com>
2023-01-16 11:02:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#Modify default IP
+sed -i 's/192.168.1.1/10.0.0.4/g' package/base-files/files/bin/config_generate
+
+#2Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
