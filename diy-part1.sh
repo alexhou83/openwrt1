5587bde513f3bd702852345e2cb3f3f0a9d36e2c
alexhou83 <120533655+alexhou83@users.noreply.github.com>
2023-01-16 11:01:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,20 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+##############################################################################################
+
+# 服务
+
+#ShadowSocksR Plus+
+#git clone https://github.com/fw876/helloworld.git package/helloworld
+
+#passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+#git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+
+#SmartDns
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+##############################################################################################
